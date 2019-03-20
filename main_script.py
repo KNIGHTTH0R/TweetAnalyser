2ed796aca787fd4126656c993e6e8965259ff751
Max <maksim.l1094@gmail.com>
2019-03-20 10:02:32 +0000

    Separated some operations into functions. New method for loading single quote JSON

--- main_script.py
+++ main_script.py
@@ -18,6 +18,7 @@ from tweet_manager import TweetManager
 from dictionary_manager import DictionaryManager
 from secret import *
 
+import ast
 import json
 import re
 
@@ -30,9 +31,18 @@ KEY_TEXT_KEYWORDS = "KEY_TEXT_KEYWORDS"
 # LOCAL GRAMMAR RULES
 rgx_rule_one = re.compile(r'TARGET-IN-NNP')
 
+def print_info(filenames):
+    print('=== FILENAMES LIST ===')
+    for i, fn in enumerate(filenames):
+        print('[{}] {}'.format(i, fn))
+
 def text_from_tweet(tweet):
-    if tweet['truncated']:
-       return tweet['extended_tweet']['full_text']
+    try:
+        if tweet['truncated']:
+            return tweet['extended_tweet']['full_text']
+
+    except KeyError:
+        pass
 
     return tweet['text']
 
@@ -47,6 +57,20 @@ def load_tweets(filename):
             data = json.loads(f.read())
     except:
         print('ERROR in load_tweets.')
+
+    return data
+
+def load_tweets_backup(filename):
+    """
+    This method loads single quoted JSON data.
+    Regular JSON expects double quotes.
+    """
+    
+    try:
+        with open(filename, 'r') as f:
+            data = ast.literal_eval(f.read())
+    except:
+        print('ERROR in load_tweets_backup.')
 
     return data
 
@@ -118,7 +142,7 @@ def local_grammar_analysis(tweets, target_word):
     # POS tagging
     st = StanfordCoreNLP(LOCATION_STARFORD_CORE_NLP)
     
-    for tweet in tweets_to_analyse:
+    for tweet in tweets:
         if target_word in tweet[KEY_TEXT_KEYWORDS]:
             # Original POS tags
             pos_tagged = st.pos_tag(tweet[KEY_TEXT_FILTERED])
@@ -135,19 +159,35 @@ def local_grammar_analysis(tweets, target_word):
             pos_string = '-'.join(pos_string)
             
             print('\n')
-
+            
+            """
             # Regex Search
             rule_one_matches = rgx_rule_one.finditer(pos_string)
             
             if rule_one_matches:
                 for match in rule_one_matches:
                     # TO-DO: If gets into this loop, match found, do something with the tweet.
-                    print(match[0])
-            
+                    print('MATCH: {}'.format(match[0]))
+            """
             print(tweet[KEY_TEXT_FILTERED])
             print(pos_string)
 
     st.close() # Do not forget to close! The backend server will consume a lot memery.
+
+def run_program(raw_tweets, keywords):
+    # Filtering
+    en_raw_tweets = language_filter(raw_tweets)
+    print('Language filter completed. Size: {}'.format(len(en_raw_tweets)))
+
+    unique_raw_tweets = duplicates_filter(en_raw_tweets)
+    print('Duplicates removed. Size: {}'.format(len(unique_raw_tweets)))
+
+    keyword_filtered = keyword_filter(unique_raw_tweets, keywords)
+    print('Keyword filtered tweets. Text and Hashtags only. Size: {}'.format(len(keyword_filtered)))
+
+    # Local Grammar Analysis
+    tweets_to_analyse = keyword_filtered
+    local_grammar_analysis(tweets_to_analyse, 'snowfall')
 
 # Main Function
 if __name__ == "__main__":
@@ -158,22 +198,15 @@ if __name__ == "__main__":
     dm = DictionaryManager()
     winter_storm_words = dm.words_from_files(winter_storm_terms_filenames)
 
-    filename = glob('./live-tweets/TweetBank/*.json')[0]
-    raw_tweets = load_tweets(filename)
+    filenames = glob('./live-tweets/TweetBank/*.json')
+
+    # Info print
+    print_info(filenames)
+    print('\n')
+
+    raw_tweets = load_tweets_backup(filenames[0])
     print('Loaded tweets. Size: {}'.format(len(raw_tweets)))
 
-    # Filtering
-    en_raw_tweets = language_filter(raw_tweets)
-    print('Language filter completed. Size: {}'.format(len(en_raw_tweets)))
-
-    unique_raw_tweets = duplicates_filter(en_raw_tweets)
-    print('Duplicates removed. Size: {}'.format(len(unique_raw_tweets)))
-
-    keyword_filtered = keyword_filter(unique_raw_tweets, winter_storm_words)
-    print('Keyword filtered tweets. Text and Hashtags only. Size: {}'.format(len(keyword_filtered)))
-
-    # Local Grammar Analysis
-    tweets_to_analyse = keyword_filtered[5:7]
-    local_grammar_analysis(tweets_to_analyse, 'snowfall')
+    run_program(raw_tweets, winter_storm_words)
 
     
